bc1995956941dcab0370a1f9f320b51404f281c3
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-06-18 08:23:06 -0600

    :tada: First commit

---
+++ src/main/java/com/cristian/chat/chat_springboot/ChatSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.chat.chat_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSpringBootApplication.class, args);
+	}
+
+}
