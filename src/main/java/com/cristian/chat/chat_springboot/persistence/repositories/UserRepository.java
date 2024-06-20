b5f48d035cef865a37becdd529f90ef05142e074
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-06-19 20:39:56 -0600

    :sparkles: Add UserRepository

---
+++ src/main/java/com/cristian/chat/chat_springboot/persistence/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.cristian.chat.chat_springboot.persistence.repositories;
+
+import com.cristian.chat.chat_springboot.persistence.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @Query(value = "SELECT * FROM user WHERE username = :username", nativeQuery = true)
+    Optional<UserEntity> findByUsername(String username);
+}
+
