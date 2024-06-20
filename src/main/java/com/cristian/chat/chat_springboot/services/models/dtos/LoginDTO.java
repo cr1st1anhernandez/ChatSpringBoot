fab113a80df1b3b22b77e805ba2575d3e9b15999
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-06-19 20:37:44 -0600

    :sparkles: Add Data transfer objects

---
+++ src/main/java/com/cristian/chat/chat_springboot/services/models/dtos/LoginDTO.java
@@ -0,0 +1,13 @@
+package com.cristian.chat.chat_springboot.services.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginDTO {
+    private String username;
+    private String password;
+}
