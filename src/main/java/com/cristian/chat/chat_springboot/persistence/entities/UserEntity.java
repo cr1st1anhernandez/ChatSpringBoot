7eb4718861114ba37c8833e5d918edb918805a4c
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-06-19 20:39:27 -0600

    :sparkles: Add UserEntity

---
+++ src/main/java/com/cristian/chat/chat_springboot/persistence/entities/UserEntity.java
@@ -0,0 +1,25 @@
+package com.cristian.chat.chat_springboot.persistence.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    private String password;
+
+}
